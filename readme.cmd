5d8f88be218a15cfe26d7d1c42a1f6c090ae731d
wangqing <2623898554@qq.com>
2018-09-26 20:39:12 +0800

    Create readme.cmd

---
+++ readme.cmd
@@ -0,0 +1,13 @@
+1.jdk下载：https://www.oracle.com/technetwork/java/javase/downloads/index.html
+
+java最新版本java11(LTS)版本今天刚刚发布，我们下载java8即可（java9,10均为过度版本），打开上面网址，往下拖，
+找到Java SE 8u181(8表示版本号，u181表示小版本号，小版本号多为安全性更新，对使用没有影响)，选择对应系统版本，
+下载安装即可（傻瓜式安装，一路下一步即可，其实就是解压一下）。
+
+2.IDE下载：
+IDEA下载地址：https://www.jetbrains.com/idea/（吃我一发安利，都8102年了，你还在用eclipse?）
+破解方法：http://idea.lanyus.com/（土豪请跳过此步，直接买）
+
+然后，就没有然后了，把代码down下载，导入成maven项目，就可以hello world了。
+
+如有问题，欢迎随时沟通哈~
